0db2f61a7c58b73f20d0d0638898b5e513960387
ddm9396 <ddm9396@gmail.com>
2022-08-12 17:34:37 +0900

    이미지 뷰 완료

---
+++ SignUp/SecondViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SecondViewController.swift
+//  SignUp
+//
+//  Created by ABLY on 2022/08/12.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    @IBOutlet weak var imageView: UIImageView!
+//    @IBAction func touchUpSelectImage(_ sender: UIImageView) {
+//        self.present(self.imagePicker,animated: true, completion: nil)
+//    }
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        self.present(self.imagePicker,animated: true,completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        let tapImageViewRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapImageViewRecognizer)
+    }
+    @IBAction func popToFirst() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{self.imageView.image = originalImage}
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ SignUp/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  SignUp
+//
+//  Created by ABLY on 2022/08/12.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func popToSecond() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
